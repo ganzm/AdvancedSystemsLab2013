e53880dc09406b3106e1f3f2e54608ea3359698f
Matthias Ganz <gaenzele@gmail.com>
2013-09-24 09:59:12 +0200

    nio test still does not work properly

--- code/mlmq/test/ch/ethz/mlmq/test/nio/SelectorTest.java
+++ code/mlmq/test/ch/ethz/mlmq/test/nio/SelectorTest.java
@@ -7,6 +7,8 @@ import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.logging.Logger;
@@ -49,9 +51,7 @@ public class SelectorTest {
 			logger.info("after select");
 
 			Set<SelectionKey> selectedKeys = selector.selectedKeys();
-
 			Iterator<SelectionKey> keyIterator = selectedKeys.iterator();
-
 			while (keyIterator.hasNext()) {
 				try {
 
@@ -105,14 +105,13 @@ public class SelectorTest {
 
 		ByteBuffer rxBuffer = connectedClient.getRxBuffer();
 
-		sChannel.write(rxBuffer);
-		logger.info("Num bytes written " + rxBuffer.position());
+		int numBytes = sChannel.write(rxBuffer);
+		logger.info("Num bytes written " + numBytes);
 
 		if (rxBuffer.remaining() <= 0) {
+			logger.info("switch to reading mode");
 			key.interestOps(SelectionKey.OP_READ);
-
 		}
-
 	}
 
 	private void read(Selector selector, SelectionKey key) throws IOException {
@@ -125,30 +124,36 @@ public class SelectorTest {
 		int byteCount = sChannel.read(rxBuffer);
 		logger.info("reading num bytes " + byteCount);
 		if (byteCount > 0) {
-			logger.info(new String(rxBuffer.array(), 0, rxBuffer.position()));
+			String request = new String(rxBuffer.array(), 0, rxBuffer.position());
+			logger.info(request);
 
 			// switch to write mode
 			key.interestOps(SelectionKey.OP_WRITE);
 
-			String htmlContent = "<html><body><h1>blub</h1></body></html>\n";
+			String htmlContent = "<html><body><h1>" + new Date() + "</h1></body></html>";
 			// @formatter:off
-			String response = 	"HTTP/1.1 200 OK\n"
-								+ "Server: Apache/1.3.29 (Unix) PHP/4.3.4" 
-								+ "Content-Type: text/html"
-								+ "Content-Lenght: " + htmlContent.getBytes().length 
-								+ "\n\n\n" 
-								+ htmlContent ;
+			String response = "HTTP/1.1 200 OK\r\n" 
+					+ "Content-Type: text/html; charset=UTF-8\r\n" 
+					+ "Content-Lenght: "
+					+ (htmlContent.getBytes(Charset.forName("UTF-8")).length) 
+					+ "\r\n"
+					+ "Connection: keep-alive\r\n"
+					+ "\r\n" 
+					+ htmlContent;
 			// @formatter:on
+
+			logger.info("Prepared Response: " + response);
+			logger.info("Response total length: " + response.getBytes(Charset.forName("UTF-8")).length);
 
 			// dirty abuse rx buffer
 			rxBuffer.clear();
-			rxBuffer.put(response.getBytes());
+			rxBuffer.put(response.getBytes(Charset.forName("UTF-8")));
 			rxBuffer.flip();
 		} else if (byteCount <= 0) {
 
+			logger.info("Socket remotely closed " + key.attachment());
 			key.cancel();
 			sChannel.close();
 		}
-
 	}
 }
