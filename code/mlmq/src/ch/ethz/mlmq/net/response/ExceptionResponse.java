31f9efb59dd204d819ea677624624ca0230d7e98
Lukas Elmer <lukas.elmer@gmail.com>
2013-09-26 14:48:24 +0200

    fix exceptionresponse

--- code/mlmq/src/ch/ethz/mlmq/net/response/ExceptionResponse.java
+++ code/mlmq/src/ch/ethz/mlmq/net/response/ExceptionResponse.java
@@ -1,6 +1,8 @@
 package ch.ethz.mlmq.net.response;
 
-public class ExceptionResponse {
+public class ExceptionResponse implements Response {
+
+	private static final long serialVersionUID = 5159033818555051347L;
 
 	public Exception getException() {
 		// TODO Auto-generated method stub
