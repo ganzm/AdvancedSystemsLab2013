28019f7bc925077ed703585cef2a7a9f29eb50ab
Matthias Ganz <gaenzele@gmail.com>
2013-10-07 16:59:16 +0200

    slightly migrated db schema

--- code/mlmq/resource/db/001_table_create.sql
+++ code/mlmq/resource/db/001_table_create.sql
@@ -1,14 +1,7 @@
-CREATE SEQUENCE broker_id START 1;
 CREATE SEQUENCE client_id START 1;
 CREATE SEQUENCE queue_id START 1;
 CREATE SEQUENCE message_id START 1;
 CREATE SEQUENCE request_response_id START 1;
-
-CREATE TABLE broker (
-  id INTEGER PRIMARY KEY DEFAULT nextval('broker_id'),
-  host VARCHAR,
-  port SMALLINT
-);
 
 
 CREATE TABLE client (
@@ -19,24 +12,20 @@ CREATE TABLE client (
 
 CREATE TABLE queue (
   id INTEGER PRIMARY KEY DEFAULT nextval('queue_id'),
-  client_id INTEGER REFERENCES client (id),
-  broker_id INTEGER NULL REFERENCES broker (id)
+  client_id INTEGER REFERENCES client (id)
 );
 
 
 CREATE TABLE message (
   id INTEGER PRIMARY KEY DEFAULT nextval('message_id'),
-  queue_id INTEGER NOT NULL REFERENCES queue (id),
+  queue_id INTEGER NOT NULL REFERENCES queue (id) ON DELETE CASCADE,
   client_sender_id INTEGER NOT NULL REFERENCES client (id),
   content VARCHAR,
   prio SMALLINT,
+  is_request_response BOOLEAN DEFAULT false,
+  request_message_id INTEGER NULL,
   sent_at time without time zone NOT NULL DEFAULT now()
 );
 
 
-CREATE TABLE request_response (
-  id INTEGER PRIMARY KEY  DEFAULT nextval('request_response_id'),
-  message_request_id INTEGER NOT NULL REFERENCES client (id),
-  message_response_id INTEGER REFERENCES client (id)
-);
 
