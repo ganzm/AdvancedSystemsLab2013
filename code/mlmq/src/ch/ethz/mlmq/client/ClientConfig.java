6d2d15d3f806fe5f4242f8eb015c999e676f4a34
Matthias Ganz <gaenzele@gmail.com>
2013-10-13 13:00:35 +0200

    added stup for ClientConfig

---
+++ code/mlmq/src/ch/ethz/mlmq/client/ClientConfig.java
@@ -0,0 +1,14 @@
+package ch.ethz.mlmq.client;
+
+public class ClientConfig {
+
+	protected String brokerHost;
+	protected int brokerPort;
+	protected String name;
+
+	/**
+	 * Scenario Nr this client should be running
+	 */
+	protected int testScenario;
+
+}
