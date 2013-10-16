95d0ac0a171d05245957501f21cdd9152d037f59
Matthias Ganz <gaenzele@gmail.com>
2013-10-16 15:10:15 +0200

    send more messages and wait some time

--- code/mlmq/src/ch/ethz/mlmq/client/testscenario/ScenarioSimpleSend.java
+++ code/mlmq/src/ch/ethz/mlmq/client/testscenario/ScenarioSimpleSend.java
@@ -1,12 +1,15 @@
 package ch.ethz.mlmq.client.testscenario;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 import ch.ethz.mlmq.client.Client;
 import ch.ethz.mlmq.client.ClientConfiguration;
 import ch.ethz.mlmq.dto.QueueDto;
+import ch.ethz.mlmq.logging.LoggerUtil;
 
 public class ScenarioSimpleSend {
+	private final Logger logger = Logger.getLogger(ScenarioSimpleSend.class.getSimpleName());
 
 	private Client client;
 	private ClientConfiguration config;
@@ -22,9 +25,15 @@ public class ScenarioSimpleSend {
 
 		QueueDto queue = client.createQueue("QueueOf" + config.getName());
 
-		for (int i = 0; i < 100; i++) {
+		for (int i = 0; i < 100000; i++) {
 			byte[] content = ("Some Random Text and message Nr " + i).getBytes();
-			client.sendMessage(queue.getId(), content, i % 10);
+			try {
+				client.sendMessage(queue.getId(), content, i % 10);
+
+				Thread.sleep(50);
+			} catch (Exception e) {
+				logger.warning("Error while sending message " + e + " " + LoggerUtil.getStackTraceString(e));
+			}
 		}
 	}
 }
