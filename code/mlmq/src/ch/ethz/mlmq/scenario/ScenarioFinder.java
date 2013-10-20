9a6c3e6c1585c90421bf1fe4c05407b9092894d9
Matthias Ganz <gaenzele@gmail.com>
2013-10-20 21:05:19 +0200

    use java util logger instead of stdout

--- code/mlmq/src/ch/ethz/mlmq/scenario/ScenarioFinder.java
+++ code/mlmq/src/ch/ethz/mlmq/scenario/ScenarioFinder.java
@@ -1,13 +1,17 @@
 package ch.ethz.mlmq.scenario;
 
+import java.util.logging.Logger;
+
 import ch.ethz.mlmq.scenario.scenarios.SimpleShutdownBroker;
 
 public class ScenarioFinder {
 
+	private final Logger logger = Logger.getLogger(ScenarioFinder.class.getSimpleName());
+
 	public Scenario<?, ?> findScenario(String scenarioName) throws Exception {
-		String s = SimpleShutdownBroker.class.getPackage().getName();
-		System.out.println(s + "." + scenarioName);
-		Class<?> c = Class.forName(s + "." + scenarioName);
+		String scenarioPkgName = SimpleShutdownBroker.class.getPackage().getName();
+		logger.info("Loading Scenario class " + scenarioPkgName + "." + scenarioName);
+		Class<?> c = Class.forName(scenarioPkgName + "." + scenarioName);
 		return (Scenario<?, ?>) c.newInstance();
 	}
 }
