c39d2bb11d06a3dceb40aa4f73c783ca0d14b0e5
Lukas Elmer <lukas.elmer@gmail.com>
2013-10-20 18:06:12 +0200

    added my position

--- code/mlmq/test/ch/ethz/mlmq/test/ScenarioConfigurationTest.java
+++ code/mlmq/test/ch/ethz/mlmq/test/ScenarioConfigurationTest.java
@@ -6,8 +6,8 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import ch.ethz.mlmq.common.InvalidConfigurationException;
 import ch.ethz.mlmq.common.BalancedScenarioConfiguration;
+import ch.ethz.mlmq.common.InvalidConfigurationException;
 import ch.ethz.mlmq.logging.LoggerUtil;
 
 public class ScenarioConfigurationTest {
@@ -20,30 +20,30 @@ public class ScenarioConfigurationTest {
 	@Test
 	public void testWithOneBroker() throws InvalidConfigurationException {
 		String m = "broker:127.0.0.1;client:127.0.0.1";
-		BalancedScenarioConfiguration s1 = new BalancedScenarioConfiguration(m, 0);
+		BalancedScenarioConfiguration s1 = new BalancedScenarioConfiguration(m, 0, "");
 		Assert.assertEquals("127.0.0.1", s1.getBrokerHost());
-		BalancedScenarioConfiguration s2 = new BalancedScenarioConfiguration(m, 1);
+		BalancedScenarioConfiguration s2 = new BalancedScenarioConfiguration(m, 1, "");
 		Assert.assertEquals("127.0.0.1", s2.getBrokerHost());
-		BalancedScenarioConfiguration s3 = new BalancedScenarioConfiguration(m, 2);
+		BalancedScenarioConfiguration s3 = new BalancedScenarioConfiguration(m, 2, "");
 		Assert.assertEquals("127.0.0.1", s3.getBrokerHost());
-		BalancedScenarioConfiguration s4 = new BalancedScenarioConfiguration(m, 3);
+		BalancedScenarioConfiguration s4 = new BalancedScenarioConfiguration(m, 3, "");
 		Assert.assertEquals("127.0.0.1", s4.getBrokerHost());
-		BalancedScenarioConfiguration s5 = new BalancedScenarioConfiguration(m, 4);
+		BalancedScenarioConfiguration s5 = new BalancedScenarioConfiguration(m, 4, "");
 		Assert.assertEquals("127.0.0.1", s5.getBrokerHost());
 	}
 
 	@Test
 	public void testWithMultipleBrokers() throws InvalidConfigurationException {
 		String m = "broker:127.0.0.1,127.0.0.2,127.0.0.3;client:127.0.0.1";
-		BalancedScenarioConfiguration s1 = new BalancedScenarioConfiguration(m, 0);
+		BalancedScenarioConfiguration s1 = new BalancedScenarioConfiguration(m, 0, "");
 		Assert.assertEquals("127.0.0.1", s1.getBrokerHost());
-		BalancedScenarioConfiguration s2 = new BalancedScenarioConfiguration(m, 1);
+		BalancedScenarioConfiguration s2 = new BalancedScenarioConfiguration(m, 1, "");
 		Assert.assertEquals("127.0.0.2", s2.getBrokerHost());
-		BalancedScenarioConfiguration s3 = new BalancedScenarioConfiguration(m, 2);
+		BalancedScenarioConfiguration s3 = new BalancedScenarioConfiguration(m, 2, "");
 		Assert.assertEquals("127.0.0.3", s3.getBrokerHost());
-		BalancedScenarioConfiguration s4 = new BalancedScenarioConfiguration(m, 3);
+		BalancedScenarioConfiguration s4 = new BalancedScenarioConfiguration(m, 3, "");
 		Assert.assertEquals("127.0.0.1", s4.getBrokerHost());
-		BalancedScenarioConfiguration s5 = new BalancedScenarioConfiguration(m, 4);
+		BalancedScenarioConfiguration s5 = new BalancedScenarioConfiguration(m, 4, "");
 		Assert.assertEquals("127.0.0.2", s5.getBrokerHost());
 	}
 
@@ -51,7 +51,7 @@ public class ScenarioConfigurationTest {
 	public void testWithNoBrokers() {
 		String m = "special:127.0.0.1,127.0.0.2,127.0.0.3;client:127.0.0.1";
 		try {
-			BalancedScenarioConfiguration s1 = new BalancedScenarioConfiguration(m, 0);
+			BalancedScenarioConfiguration s1 = new BalancedScenarioConfiguration(m, 0, "");
 			Assert.assertEquals("127.0.0.1", s1.getBrokerHost());
 			Assert.fail("Expected InvalidConfigurationException");
 		} catch (InvalidConfigurationException e) {
@@ -62,7 +62,7 @@ public class ScenarioConfigurationTest {
 	@Test
 	public void testBrokerPort() throws InvalidConfigurationException {
 		String m = "special:127.0.0.1,127.0.0.2,127.0.0.3;client:127.0.0.1";
-		BalancedScenarioConfiguration s = new BalancedScenarioConfiguration(m, 0);
+		BalancedScenarioConfiguration s = new BalancedScenarioConfiguration(m, 0, "");
 		Assert.assertEquals(8099, s.getBrokerPort());
 	}
 }
