000962c26d2a59a071af8520b0a85133f0facf04
Lukas Elmer <lukas.elmer@gmail.com>
2013-11-04 21:50:41 +0100

    refactoring

--- code/log_analizer/src/ch/ethz/mlmq/log_analizer/LogAnalizer.java
+++ code/log_analizer/src/ch/ethz/mlmq/log_analizer/LogAnalizer.java
@@ -6,14 +6,11 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
 
 public class LogAnalizer {
 
-	private Map<String, ArrayList<Bucket>> bucketsByName = new HashMap<String, ArrayList<Bucket>>();
 	private ArrayList<File> files = new ArrayList<>();
 
 	public void addFile(String testLogPath) {
