4f4dd276cb440cfe00b3568d47d71258e226c213
Lukas Elmer <lukas.elmer@gmail.com>
2013-11-11 10:17:51 +0100

    failsafe

--- code/log_analyzer/src/ch/ethz/mlmq/log_analyzer/LogLineParser.java
+++ code/log_analyzer/src/ch/ethz/mlmq/log_analyzer/LogLineParser.java
@@ -5,8 +5,13 @@ public class LogLineParser {
 	public static LogLine parseLogLine(String firstLine) {
 		String[] s = firstLine.split(";");
 
-		String s3 = s.length == 3 ? "" : s[3];
-		return new LogLine(Integer.parseInt(s[0]), Long.parseLong(s[1]), s[2], s3);
+		if (s.length >= 3) {
+			String s3 = s.length == 3 ? "" : s[3];
+			return new LogLine(Integer.parseInt(s[0]), Long.parseLong(s[1]), s[2], s3);
+		} else {
+			System.out.println("Unable to parse log line " + firstLine);
+			return new LogLine(0, Long.MIN_VALUE, "", "");
+		}
 	}
 
 }
