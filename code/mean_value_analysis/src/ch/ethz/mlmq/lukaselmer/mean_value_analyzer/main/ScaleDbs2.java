7f03fae80f924d06f45674f040b2b7c6f24d8e28
Lukas Elmer <lukas.elmer@gmail.com>
2013-12-20 02:55:59 +0100

    ms2

---
+++ code/mean_value_analysis/src/ch/ethz/mlmq/lukaselmer/mean_value_analyzer/main/ScaleDbs2.java
@@ -0,0 +1,42 @@
+package ch.ethz.mlmq.lukaselmer.mean_value_analyzer.main;
+
+import ch.ethz.mlmq.lukaselmer.mean_value_analyzer.queue.Queue;
+import ch.ethz.mlmq.lukaselmer.mean_value_analyzer.queue.QueueType;
+import ch.ethz.mlmq.lukaselmer.mean_value_analyzer.solver.MeanValueAnalyzer;
+
+public class ScaleDbs2 {
+
+	public static void main(String[] args) {
+
+		boolean printResponseTime = false;
+		int dbCount = 30;
+
+		boolean printBottleneck = false;
+		double middlewareTime = 0.042;
+		int maxClients = 2500;
+		double Z = 10;
+
+		double tcpTime = 2.617;
+		int middlewareCount = 80;
+		int dbWorkers = 8 * dbCount;
+		double dbServiceTime = 4.506; // fixed value (good): 4.506, estimated value (bad): 5.106
+
+		MeanValueAnalyzer mva = new MeanValueAnalyzer();
+
+		for (int numClients = 50; numClients <= maxClients; numClients += 50) {
+			mva.addQueue(new Queue("TCP 1", tcpTime, 1, QueueType.delayCenter, 1, maxClients + 1));
+			mva.addQueue(new Queue("TCP 2", tcpTime, 1, QueueType.delayCenter, 1, maxClients + 1));
+			mva.addQueue(new Queue("TCP 3", tcpTime, 1, QueueType.delayCenter, 1, maxClients + 1));
+			mva.addQueue(new Queue("TCP 4", tcpTime, 1, QueueType.delayCenter, 1, maxClients + 1));
+
+			for (int i = 1; i <= middlewareCount; i++) {
+				mva.addQueue(new Queue("Middleware " + i, middlewareTime, 1.0 / (double) middlewareCount, QueueType.loadDependent, 1, maxClients + 1));
+			}
+
+			mva.addQueue(new Queue("Database", dbServiceTime, 1.0, QueueType.loadDependent, dbWorkers, maxClients + 1));
+
+			mva.calculateNetwork(numClients, Z, numClients == maxClients && printBottleneck, printResponseTime);
+			mva.clearQueues();
+		}
+	}
+}
